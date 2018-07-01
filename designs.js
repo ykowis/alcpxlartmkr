93ef82d6048c8e25faa89eae8ec5608e31a05eec
Femi Siwoku <siwoku.femi@gmail.com>
2018-07-01 23:33:57 +0100

    second commit

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  let inputHeight = $('#inputHeight').val();
+  let inputWidth = $('#inputWidth').val();
 
-// Your code goes here!
+  $('#pixelCanvas').html('');
 
+  for (let i = 1; i <= inputWidth; i++){
+    $('#pixelCanvas').append('<tr id = row' + i + '></tr>');
+    for (let j = 1; j <= inputHeight; j++) {
+      $('#row' + i).append('<td id=coloumn' + j + '></td>');
+    }
+  }
+
+  $('#sizePicker').on('submit', function(Event){
+    Event.preventDefault()
+    makeGrid();
+  });
+
+  $('td').click(highlight);
+
+  function highlight() {
+    let color = $('#colorPicker').val();
+    ($(this).attr('style')) ? $(this).removeAttr('style') : $(this).attr('style', 'background-color:${color}');
+
+  }
 }
